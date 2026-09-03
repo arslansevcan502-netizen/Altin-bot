8545b0115090fba140ee8070a490419e496b2117
arslansevcan502-netizen <arslansevcan502@gmail.com>
2026-09-03 04:31:21 +0300

    Create check_gold.py

---
+++ check_gold.py
@@ -0,0 +1,18 @@
+import requests
+import os
+
+TOPIC = os.environ["NTFY_TOPIC"]
+
+data = requests.get("https://finans.truncgil.com/today.json").json()
+
+gram_altin = data["gram-altin"]
+alis = gram_altin["Alış"]
+satis = gram_altin["Satış"]
+
+mesaj = f"Gram Altın\nAlış: {alis} TL\nSatış: {satis} TL"
+
+requests.post(
+    f"https://ntfy.sh/{TOPIC}",
+    data=mesaj.encode("utf-8"),
+    headers={"Title": "Altın Fiyatı"}
+)
